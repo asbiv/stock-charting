275947a3180d55448a8c0924ab57f908babe1599
asbiv <boxley4@gmail.com>
2016-02-26 10:27:45 -0500

    Update ggSeriesData_v1.R

--- stockAnalyzer_v2/ggSeriesData_v1.R
+++ stockAnalyzer_v2/ggSeriesData_v1.R
@@ -4,44 +4,43 @@ ggSeriesData <- function(symbol.list, start, end){
   
   #Loop through symbols, fetch prices, and store in symbol.list
   x.list <- list()
-  x.list <-lapply(symbol.list, function(x) {getSymbols(x, auto.assign=FALSE, options("getSymbols.warning4.0"=FALSE))}) #Removed from=start, to=end,
+  x.list <-lapply(symbol.list, function(x) {getSymbols(x, auto.assign=FALSE, options("getSymbols.warning4.0"=FALSE))})
   names(x.list) <- symbol.list
   
   #Create the sub-lists
   x.subset <- lapply(1:length(x.list), function(x){
-    #First, strip OHLC data (need to vectorize)
     {date <- as.Date(time(x.list[[x]]))
     open <- as.vector(Op(x.list[[x]]))
     high <- as.vector(Hi(x.list[[x]]))
     low <- as.vector(Lo(x.list[[x]]))
     close <- as.vector(Cl(x.list[[x]]))
     
-    #Then build the data frame
+    #Build DF
     temp <- data.frame('date'=date,'open'=open,'high'= high,'low'=low,'close'=close)
     
-    #Add the symbol
+    #Add Symbol
     temp$symbol <- as.character(names(x.list[x]))
     
-    #We want to construct our candlesticks  
+    #Candlestick components
     temp$candleLower <- pmin(temp$open, temp$close)
     temp$candleMiddle <- (temp$open + temp$close)/2 #NA #Can't use NA 
     temp$candleUpper <- pmax(temp$open, temp$close)
     temp$fill <- ''
-    temp$fill[temp$open < temp$close] = "Gain" #'#31a354'
-    temp$fill[temp$fill ==''] = "Loss" #'#de2d26'
+    temp$fill[temp$open < temp$close] = "Gain"
+    temp$fill[temp$fill ==''] = "Loss"
     
     #Add Moving Averages
-'    long <- as.numeric(end-start)
-    short <- as.numeric(end-start)/4'
     temp$maLong <- SMA(temp$close, n=200)
     temp$maShort <- SMA(temp$close, n=50)}
     
+    #Subset by date
     temp <-subset(temp, temp$date > start & temp$date < end)
     
     return(temp)
   })
   
   names(x.subset) <- symbol.list
+  #Merge to single DF
   x.merged <- Reduce(function(...) merge(..., all=T), x.subset)
   return(x.merged)
 }
